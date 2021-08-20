16cb6ba890c738b7347af9cf853a6b24079e3a55
Alisha Edington <alisha_56789@yahoo.com>
2021-08-19 22:16:01 -0600

    changed router to deal with the loaderio

--- backend/server/index.js
+++ backend/server/index.js
@@ -9,12 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/loaderio-cd147b6287330d187ac35365492131ce/', (req, res) => {
-  console.log(req);
-  res.send('loaderio-cd147b6287330d187ac35365492131ce');
-});
+app.use('/', router);
 
-app.use('/reviews', router);
 app.listen(port, () => {
   console.log(`We be listening, port: ${port}`);
 });
--- backend/server/routes.js
+++ backend/server/routes.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const controller = require('./controllers');
 
-router.get('/', controller.getReviews);
+router.get('/reviews', controller.getReviews);
 
-router.get('/meta', controller.getReviewsMeta);
+router.get('/reviews/meta', controller.getReviewsMeta);
 
-router.post('/', controller.postReview);
+router.post('/reviews', controller.postReview);
 
-router.put('/:review_id/helpful', controller.helpful);
+router.put('/reviews/:review_id/helpful', controller.helpful);
 
-router.put('/:review_id/report', controller.report);
+router.put('/reviews/:review_id/report', controller.report);
+
+router.get('/loaderio-cd147b6287330d187ac35365492131ce/', (req, res) => {
+  res.send('loaderio-cd147b6287330d187ac35365492131ce');
+});
 
 module.exports = router;
