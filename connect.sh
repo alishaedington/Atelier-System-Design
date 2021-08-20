173711dc7a95cd8375b4dcbf90b5f9c698d95b1f
Alisha Edington <alisha_56789@yahoo.com>
2021-08-20 17:25:04 -0600

    add nginx dockerfile and config.

--- connect.sh
+++ connect.sh
@@ -9,4 +9,9 @@ node)
     scp -i SDC.pem ./init.sh ec2-user@ec2-3-144-8-8.us-east-2.compute.amazonaws.com:~/init.sh
     ssh -i SDC.pem ec2-user@ec2-3-144-8-8.us-east-2.compute.amazonaws.com
     ;;
+
+nginx)
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com
+    ;;
 esac
