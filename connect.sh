c15eebc31a9e802d98b159355f8a49191e6be87b
Alisha Edington <alisha_56789@yahoo.com>
2021-08-21 15:00:34 -0600

    connection updated with second node

--- connect.sh
+++ connect.sh
@@ -10,6 +10,11 @@ node)
     ssh -i SDC.pem ec2-user@ec2-3-144-8-8.us-east-2.compute.amazonaws.com
     ;;
 
+node2)
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-222-176-86.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-222-176-86.us-east-2.compute.amazonaws.com
+    ;;
+
 nginx)
     scp -i SDC.pem ./init.sh ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com:~/init.sh
     ssh -i SDC.pem ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com
