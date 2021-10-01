ae8c69a3cb08e5332e1ae1c657104ab2e10d5ee0
Alisha Edington <alisha_56789@yahoo.com>
2021-10-01 12:41:30 -0600

    Restarted instances, changed IP's to match, added caching to nginx LB

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,12 +3,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'alishaedington',
-  host: 'localhost', // use when running locally
-  // host: '52.15.183.253', // use when running in ec2 instance (check IP matchs IP of instance)
+  // host: 'localhost', // use when running locally
+  host: '18.189.29.23', // use when running in ec2 instance (check IP matchs IP of instance)
   database: 'reviews',
   password: 'alishaedington',
-  port: 5432, // use when running locally
-  // port: 3002,
+  // port: 5432, // use when running locally
+  port: 3002,
 });
 
 module.exports = pool;
--- connect.sh
+++ connect.sh
@@ -1,22 +1,22 @@
 case $1 in
 
 postgres)
-    scp -i SDC.pem ./init.sh ec2-user@ec2-52-15-183-253.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i SDC.pem ec2-user@ec2-52-15-183-253.us-east-2.compute.amazonaws.com
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-189-29-23.us-east-2.compute.amazonaws.com	:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-189-29-23.us-east-2.compute.amazonaws.com
     ;;
 
 node)
-    scp -i SDC.pem ./init.sh ec2-user@ec2-3-144-8-8.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i SDC.pem ec2-user@ec2-3-144-8-8.us-east-2.compute.amazonaws.com
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-119-117-71.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-119-117-71.us-east-2.compute.amazonaws.com
     ;;
 
 node2)
-    scp -i SDC.pem ./init.sh ec2-user@ec2-18-222-176-86.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i SDC.pem ec2-user@ec2-18-222-176-86.us-east-2.compute.amazonaws.com
+    scp -i SDC.pem ./init.sh ec2-user@ec2-3-129-62-202.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-3-129-62-202.us-east-2.compute.amazonaws.com
     ;;
 
 nginx)
-    scp -i SDC.pem ./init.sh ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i SDC.pem ec2-user@ec2-18-188-42-169.us-east-2.compute.amazonaws.com
+    scp -i SDC.pem ./init.sh ec2-user@ec2-18-216-29-73.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-18-216-29-73.us-east-2.compute.amazonaws.com
     ;;
 esac
