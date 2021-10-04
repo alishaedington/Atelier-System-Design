f2848ddf7cdf664b185b5fe7467bb866bb5c0271
Alisha Edington <alisha_56789@yahoo.com>
2021-10-04 13:30:15 -0600

    adding third server

--- connect.sh
+++ connect.sh
@@ -15,6 +15,16 @@ node2)
     ssh -i SDC.pem ec2-user@ec2-3-129-62-202.us-east-2.compute.amazonaws.com
     ;;
 
+node3)
+    scp -i SDC.pem ./init.sh ec2-user@ec2-52-15-122-106.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-52-15-122-106.us-east-2.compute.amazonaws.com
+    ;;
+
+node4)
+    scp -i SDC.pem ./init.sh ec2-user@ec2-3-129-62-202.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i SDC.pem ec2-user@ec2-3-129-62-202.us-east-2.compute.amazonaws.com
+    ;;
+
 nginx)
     scp -i SDC.pem ./init.sh ec2-user@ec2-18-216-29-73.us-east-2.compute.amazonaws.com:~/init.sh
     ssh -i SDC.pem ec2-user@ec2-18-216-29-73.us-east-2.compute.amazonaws.com
